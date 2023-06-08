99f8e22f8577e17d5ea23e853c6f0499d56340da
Seungduk Seo <seseo@student.42seoul.kr>
2023-06-08 21:51:40 +0900

    solved

---
+++ Python-0-Starting/ex04/whatis.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def whatis(args):
+    if len(args) > 2:
+        raise AssertionError("more than one argument are provided")
+    elif len(args) == 2:
+        try:
+            arg = float(args[1])
+            if arg == int(arg):
+                if arg % 2 == 1:
+                    print("I'm Odd.")
+                else:
+                    print("I'm Even.")
+            else:
+                raise AssertionError("argument is not an integer")
+        except ValueError as e:
+            raise AssertionError("argument is not an integer")
+
+
+if __name__ == '__main__':
+    try:
+        whatis(sys.argv)
+    except AssertionError as e:
+        print("AssertionError:", e, file=sys.stderr)
+    except Exception as e:
+        print(e.with_traceback, file=sys.stderr)
