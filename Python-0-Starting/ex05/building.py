bad8fece086e083025689622b169100115567e2b
Seungduk Seo <seseo@student.42seoul.kr>
2023-06-11 23:26:31 +0900

    solved

---
+++ Python-0-Starting/ex05/building.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""
+building main
+"""
+
+import sys
+import string
+
+
+def main():
+    try:
+        punc = string.punctuation
+        ret = [0, 0, 0, 0, 0]
+        if len(sys.argv) == 1:
+            print("What is the text to count?")
+            input = sys.stdin.readline()
+        elif len(sys.argv) == 2:
+            input = sys.argv[1]
+        else:
+            raise AssertionError("Too many args")
+        for c in input:
+            if c.isalpha():
+                if c == c.capitalize():
+                    ret[0] += 1
+                else:
+                    ret[1] += 1
+            elif c in punc:
+                ret[2] += 1
+            elif c.isspace():
+                ret[3] += 1
+            elif c.isdigit():
+                ret[-1] += 1
+        print("The text contains {strlen} characters:".format(
+            strlen=len(input)))
+        print("{upper} upper letters".format(upper=ret[0]))
+        print("{lower} lower letters".format(lower=ret[1]))
+        print("{punctuation} punctuation mark".format(punctuation=ret[2]))
+        print("{spaces} spaces".format(spaces=ret[3]))
+        print("{digits} digits".format(digits=ret[-1]))
+    except AssertionError as e:
+        print("AssertionError:", e, file=sys.stderr)
+    except Exception as e:
+        print("Error:", e, file=sys.stderr)
+
+
+if __name__ == "__main__":
+    # print(__doc__)
+    main()
