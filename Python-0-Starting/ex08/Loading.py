4e7008397715cd4043cfc335576df19a326e81b0
Seungduk Seo <seseo@student.42seoul.kr>
2023-06-14 00:31:09 +0900

    ft_tqdm implemented. flake8 check required

--- Python-0-Starting/ex08/Loading.py
+++ Python-0-Starting/ex08/Loading.py
@@ -1,15 +1,102 @@
 #!/usr/bin/python3
 
 """
-Loading
+ft_tqdm(lst: range) -> None:
+
+this function will display processing bar.
 """
 
-
-import sys
-
-
-# def ft_tqdm(lst: range) -> None:
+import shutil
+import time
 
 
-# if __name__ == "__main__":
-#     print(__doc__)
+def ft_tqdm(lst: range) -> None:
+    column_space = shutil.get_terminal_size().columns
+    total = len(lst)
+    last_it_time = start_time = time.time()
+    spit = 0
+
+    spit_acc = 0
+
+    for i in range(total):
+        if i == 0:
+            bar_str_s = "  0%|"
+            bar_str_e = f"| 0/{total} [00:00<?, ?it/s]"
+            bar_space = column_space - len(bar_str_s) - len(bar_str_e)
+            if bar_space <= 0:
+                bar = ""
+            else:
+                bar = " " * bar_space
+            print("\r" + bar_str_s + bar + bar_str_e, end="", flush=True)
+        else:
+            cur_it_time = time.time()
+            spit = cur_it_time - last_it_time
+            last_it_time = cur_it_time
+
+            elapsed_time = int(cur_it_time - start_time)
+            elapsed_time_hour = elapsed_time // 3600
+            elapsed_time_min = (elapsed_time % 3600) // 60
+            elapsed_time_sec = elapsed_time % 60
+
+            est_end_time = int((total - i) * (elapsed_time / i))
+            est_end_time_hour = est_end_time // 3600
+            est_end_time_min = (est_end_time % 3600) // 60
+            est_end_time_sec = est_end_time % 60
+
+            percentage = i / total * 100
+
+            bar_str_s = f"{int(percentage):3}%|"
+            bar_str_e = f"| {i}/{total} "
+
+            if elapsed_time_hour:
+                bar_str_e += f"[{elapsed_time_hour:02}:{elapsed_time_min:02}:{elapsed_time_sec:02}<"
+            else:
+                bar_str_e += f"[{elapsed_time_min:02}:{elapsed_time_sec:02}<"
+            if est_end_time_hour:
+                bar_str_e += f"{est_end_time_hour:02}:{est_end_time_min:02}:{est_end_time_sec:02}, "
+            else:
+                bar_str_e += f"{est_end_time_min:02}:{est_end_time_sec:02}, "
+            if spit < 1:
+                bar_str_e += f"{1/spit:5.2f}it/s]"
+            else:
+                bar_str_e += f"{spit:5.2f}s/it]"
+
+            bar_space = column_space - len(bar_str_s) - len(bar_str_e)
+            if bar_space <= 0:
+                bar = ""
+            else:
+                bar = int(percentage / 100 * bar_space) * "█"
+                bar += " " * (bar_space - len(bar))
+            spit_acc += spit
+            if spit_acc > 0.1:
+                print("\r" + bar_str_s + bar + bar_str_e, end="", flush=True)
+                spit_acc = 0
+
+        yield None
+
+    else:
+        cur_it_time = time.time()
+        elapsed_time = int(cur_it_time - start_time)
+        elapsed_time_hour = elapsed_time // 3600
+        elapsed_time_min = (elapsed_time % 3600) // 60
+        elapsed_time_sec = elapsed_time % 60
+
+        bar_str_s = "100%|"
+        bar_str_e = f"| {total}/{total} "
+        if elapsed_time_hour:
+            bar_str_e += f"[{elapsed_time_hour:02}:{elapsed_time_min:02}:{elapsed_time_sec:02}"
+        else:
+            bar_str_e += f"[{elapsed_time_min:02}:{elapsed_time_sec:02}"
+        bar_str_e += "<00:00, "
+        if spit < 1:
+            bar_str_e += f"{1/spit:5.2f}it/s]"
+        else:
+            bar_str_e += f"{spit:5.2f}s/it]"
+
+        bar_space = column_space - len(bar_str_s) - len(bar_str_e)
+        bar = "█" * bar_space
+        print("\r" + bar_str_s + bar + bar_str_e)
+
+
+if __name__ == "__main__":
+    print(__doc__)
--- Python-0-Starting/ex08/tester.py
+++ Python-0-Starting/ex08/tester.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
 
 """
-tester
+tester for ft_tqdm from Loading.py
 """
 
 
 from time import sleep
 from tqdm import tqdm
+from Loading import ft_tqdm
 
-# from Loading import ft_tqdm
-
-# for elem in ft_tqdm(range(333)):
-#     sleep(0.005)
-#     print()
+for elem in ft_tqdm(range(333)):
+    sleep(0.005)
+print()
 
 for elem in tqdm(range(333)):
     sleep(0.005)
-print("")
+print()
+
+for elem in ft_tqdm(range(10)):
+    sleep(1)
+print()
+
+for elem in tqdm(range(10)):
+    sleep(1)
+print()
