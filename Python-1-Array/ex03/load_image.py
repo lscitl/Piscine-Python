f365ff0d7a99786ea8fd3e3c1c7fd2cbad1c0812
Seungduk Seo <seseo@student.42seoul.kr>
2023-06-16 22:49:26 +0900

    solving

---
+++ Python-1-Array/ex03/load_image.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import sys
+from PIL import Image
+import numpy as np
+
+
+def ft_load(path: str) -> np.array:
+    """
+    def ft_load(path: str)
+
+    load image to numpy array
+    """
+
+    try:
+        if type(path) != str:
+            raise AssertionError("invalid input type")
+        image = Image.open(path)
+        data = np.asarray(image)
+        print("The shape of image is:", data.shape)
+        return data
+    except Exception as e:
+        print("Error:", e, file=sys.stderr)
+
+
+if __name__ == "__main__":
+    print(ft_load.__doc__)
