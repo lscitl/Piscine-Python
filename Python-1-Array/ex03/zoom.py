3c67d20322cd3adc65e4b65aacb365297f05037e
Seungduk Seo <seseo@student.42seoul.kr>
2023-06-22 00:04:43 +0900

    wip

--- Python-1-Array/ex03/zoom.py
+++ Python-1-Array/ex03/zoom.py
@@ -4,6 +4,7 @@ import sys
 from load_image import ft_load
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -20,11 +21,31 @@ def main():
         print("Error:", e, file=sys.stderr)
         return
     
-    zoomed_img = image[50:50+400][500:900]
-    Image.fromarray(zoomed_img).show()
+    p = [100, 450]
+    # p = [767, 1023]
+    zoom_size = [400, 400]
     
-    return
+    # 400 x 400
+    try:
+        zoomed_img = image[p[0]:p[0] + zoom_size[0], p[1]:p[1] + zoom_size[1]]
+        if zoomed_img.size == 0:
+            raise AssertionError("Nothing to zoom.")
+    except Exception as e:
+        print("Error:", e, file=sys.stderr)
+        return
 
+    # expand dimension
+    reshape_img = np.expand_dims(zoomed_img, axis=-1)
+
+    size1 = str(reshape_img.shape)
+    size2 = str(zoomed_img.shape)
+    
+    print("New shape after slicing:", size1, "or", size2)
+    print(reshape_img)
+    plt.imshow(reshape_img, 'gray')
+    plt.show()
+    # Image.fromarray(zoomed_img).show()
+    
 
 if __name__ == "__main__":
     main()
