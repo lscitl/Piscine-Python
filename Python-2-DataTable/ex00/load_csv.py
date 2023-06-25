1e80421257a9add985c9b1976ae4e7bcf711b720
Seungduk Seo <seseo@student.42seoul.kr>
2023-06-25 23:16:51 +0900

    solved

---
+++ Python-2-DataTable/ex00/load_csv.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import sys
+import pandas as pd
+
+
+def load(path: str) -> pd.DataFrame:
+    """
+    def load(path: str) -> pd.DataFrame:
+
+    load data to pandas dataset
+    """
+
+    try:
+        if type(path) != str:
+            raise AssertionError("invalid input type")
+        data = pd.read_csv(path)
+        print("Loading dataset of dimensions:", data.shape)
+        return data
+    except AssertionError as e:
+        print("AssertionError:", e, file=sys.stderr)
+
+    except Exception as e:
+        print("Error:", e, file=sys.stderr)
+
+
+def main():
+    print(load("../life_expectancy_years.csv"))
+
+if __name__ == "__main__":
+    main()
+    print(load.__doc__)
---
+++ Python-2-DataTable/ex00/tester.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+from load_csv import load
+print(load("life_expectancy_years.csv"))
