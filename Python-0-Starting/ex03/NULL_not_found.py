80dcd8e1c11d2867d447cf22f45d104ad2b6e708
Seungduk Seo <seseo@student.42seoul.kr>
2023-08-06 18:59:52 +0900

    function updated

--- Python-0-Starting/ex03/NULL_not_found.py
+++ Python-0-Starting/ex03/NULL_not_found.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
 
-def is_null(func) -> int:
-    def inner(obj):
-        if (obj and type(obj) != float) \
-                or (type(obj) == float and str(obj) != 'nan'):
+def is_null(func) -> object:
+    def inner(obj) -> int | object:
+        if (obj and type(obj) is not float) \
+                or (type(obj) is float and str(obj) != 'nan'):
             print("Type not Found")
             return 1
-        elif obj is None:
-            print("Nothing:", end=' ')
-        elif obj == 0:
-            print("Zero:", end=' ')
-        elif obj == '':
-            print("Empty:", end=' ')
-        elif obj is False:
-            print("Fake:", end=' ')
-        else:
-            print("Cheese:", end=' ')
         return func(obj)
     return inner
 
 
 @is_null
 def NULL_not_found(object: any) -> int:
+    d = {
+            type(None): "Nothing:",
+            int: "Zero:",
+            str: "Empty:",
+            bool: "Fake:",
+            float: "Cheese:"
+        }
     if object != '':
-        print(object, type(object))
+        print(d[type(object)], object, type(object))
     else:
-        print(type(object))
+        print(d[type(object)], type(object))
     return 0
